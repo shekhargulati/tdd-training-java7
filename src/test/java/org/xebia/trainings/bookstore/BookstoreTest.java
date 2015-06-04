af2787efcf9a62cbf1c3a2e479fac49aad36b7a7
Shekhar Gulati <shekhargulati84@gmail.com>
2015-06-04 21:10:49 +0530

    Project initial setup done

---
+++ src/test/java/org/xebia/trainings/bookstore/BookstoreTest.java
@@ -0,0 +1,14 @@
+package org.xebia.trainings.bookstore;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BookstoreTest {
+
+	@Test
+	public void projectInitialSetupShouldWork() {
+		assertTrue("project dependency should be met.", true);
+	}
+
+}
